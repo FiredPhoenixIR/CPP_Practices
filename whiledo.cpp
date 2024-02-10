017f6c8573863de76cbbfaf434f5e86aaf0f1866
Mahdi ST <84761387+FiredPhoenixIR@users.noreply.github.com>
2024-02-10 17:20:26 +0330

    " Loops "

---
+++ whiledo.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	for (int x = 5; x < 10; x++) {
+		cout << x << endl;
+	}
+
+	int i = 10;
+	while (i < 18) {
+		i++;
+		cout << i << endl;
+	}
+
+	int y = 20;
+	do {
+		cout << y << endl;
+		y++;
+	}
+		while (y < 20);
+
+	for (int z = 30;z < 35;z++) {
+		if (z == 32) {
+			continue; //break;
+		}
+		cout << " z is : " << z << endl;
+	}
+
+}
