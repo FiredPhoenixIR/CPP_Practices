a9e9e4a02a2b4890f1ff8dc2226660d6cb0729a0
Mahdi ST <84761387+FiredPhoenixIR@users.noreply.github.com>
2024-02-11 19:00:05 +0330

    " max item 3d array + sum fix "

---
+++ array_maxitem.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int max_item_arr(int x[], int a, int b, int c)
+{
+	int max_item = x[0];
+	for (int i = 0; i < a;i++) { // looping through plates
+		for (int ii = 0;ii < b;ii++) { // looping through rows
+			for (int iii = 0;iii < c;iii++) { //looping through cols
+				if (x[i * b * c + ii * c + iii] > max_item)
+					max_item = x[i * b * c + ii * c + iii];
+			}
+		}
+	}
+	return max_item;
+}
+
+int main() {
+	int ex_arr[3][2][4] = { 1,2,3,4 ,5,6,7,8  ,9,10,11,12 ,13,14,15,16  ,17,18,19,20 ,21,22,23,24 };
+	int result = max_item_arr(*ex_arr[0], 3, 2, 4);
+	cout << result << endl;
+}
