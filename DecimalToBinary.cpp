e56aea2200b4d75365ab5a2731f7275bf99b3339
Mahdi ST <84761387+FiredPhoenixIR@users.noreply.github.com>
2024-03-22 05:25:51 +0330

    " Some good practices "

---
+++ DecimalToBinary.cpp
@@ -0,0 +1,18 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//int main() {
+//    int binary = 0;
+//    int remainder, product = 1;
+//    int decimal = 6;
+//
+//    while (decimal != 0) {
+//        remainder = decimal % 2;
+//        binary += remainder * product;
+//        decimal = decimal / 2;
+//        product *= 10;
+//    }
+//
+//    cout << binary;
+//}
---
+++ Switch.cpp
@@ -0,0 +1,48 @@
+//#include<iostream>
+//using namespace std;
+//
+////int main() {
+////    int percentage = 85;
+////
+////    cout << "Grade : ";
+////    switch (percentage) {
+////    case 90 ... 100:
+////        cout << "A";
+////        break;
+////    case 70 ... 89:
+////        cout << "B";
+////        break;
+////    case 50 ... 69:
+////        cout << "C";
+////        break;
+////    default:
+////        cout << "D";
+////    }
+////}
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    int percentage = 85;
+//
+//    cout << "Grade : ";
+//
+//    switch (percentage / 10) {
+//    case 10:
+//    case 9:
+//        cout << "A";
+//        break;
+//    case 8:
+//    case 7:
+//        cout << "B";
+//        break;
+//    case 6:
+//    case 5:
+//        cout << "C";
+//        break;
+//    default:
+//        cout << "D";
+//    }
+//}
